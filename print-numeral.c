93f198d48efa5b1382fa6df42dd63542d68c8cf5
serenacodes450 <Shinyemmy@gmail.com>
2025-04-01 16:13:37 +0530

    trying the escape sequence thingie

---
+++ print-numeral.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    /*This is my first time learning c.
+     I was forced by my sister so. YAYYYYY*/
+     printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+     printf("This is something.Dont ask");
+     return 0;
+}
