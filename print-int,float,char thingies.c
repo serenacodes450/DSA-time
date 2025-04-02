7c3650440617464136afc3f192226f9c15b78c3b
serenacodes450 <Shinyemmy@gmail.com>
2025-04-02 17:39:11 +0530

    using variables

---
+++ print-int,float,char thingies.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int age= 14;
+    float gpa=6.23;
+    char grade='A';
+    char name[]="Serena";
+    printf("Hey %s!\n",name);
+    printf("You are %d years old.\n",age);
+    printf("Your average grade is %c\n",grade);
+    printf("Your gpa is %f.",gpa);
+    return 0;
+}
