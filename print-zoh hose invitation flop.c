f528e78052a0f4b25df7eeff2fbe996543d85b27
serenacodes450 <Shinyemmy@gmail.com>
2025-04-02 20:21:27 +0530

    my lore ig?

---
+++ print-zoh hose invitation flop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    printf("Zoha called amma to invite me to her house.\n");
+    printf("Btw,zoha is one of my bffs of 4 yrs.\n");
+    printf("My mom and Dad told me that I cannot go to her house.\n");
+    printf("Awww,shucks!\n");
+    printf("Btw, Bebo,Bella and Suzy is going.\n");
+    printf("Aww man!!!!!!");
+    return 0;
+}
