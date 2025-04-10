a3257fa8f4efb8a098e453f4021be256683db84c
serenacodes450 <Shinyemmy@gmail.com>
2025-04-10 09:43:19 +0530

    ehhhh

---
+++ print-float.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float item1=56.12;
+    float item2=63.64;
+    float item3=76.42;
+    printf("Item 1 :$%.2f\n",item1);
+    printf("Item 2 :$%.2f\n",item2);
+    printf("Item 3 :$%.2f\n",item3);
+    return 0;
+    
+}
